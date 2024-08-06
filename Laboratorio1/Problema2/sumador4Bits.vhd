665b66922c977beafddd661b7f6b65fee94cd4c3
Giancarlo Vega Marin <jungianca6@estudiantec.cr>
2024-08-06 12:01:44 -0600

    intento de display
    pendiente de resolver

--- Laboratorio1/Problema2/sumador4Bits.vhd
+++ Laboratorio1/Problema2/sumador4Bits.vhd
@@ -7,7 +7,10 @@ entity sumador4Bits is
 		  B: in std_logic_vector(3 downto 0); 			-- Entrada del segundo número de 4 bits
         Cin  : in std_logic;                   		-- Acarreo de entrada
         S : out std_logic_vector(3 downto 0); 	-- Salida de la suma de 4 bits
-        Cout : out std_logic);  -- Salida del acarreo de salida
+        Cout : out std_logic;							-- Salida del acarreo de salida
+		  sum_seg: out std_logic_vector(6 downto 0);
+        carry_seg: out std_logic_vector(6 downto 0)
+		  );  
 end sumador4Bits;
 
 architecture sumadorCompleto4Bits of sumador4Bits is
@@ -24,13 +27,15 @@ architecture sumadorCompleto4Bits of sumador4Bits is
         
     end component;
 	 
-	 --component disp7seg is
+	 component disp7seg is
 	 
-		--port (b_num : in  STD_LOGIC_VECTOR (3 downto 0);
-				--seg : out  STD_LOGIC_VECTOR (6 downto 0) );
-	
+		port (b_num : in  STD_LOGIC_VECTOR (3 downto 0);
+				seg : out  STD_LOGIC_VECTOR (6 downto 0) );
+				
+	 end component;
 	 
-	 signal f_carry : std_logic_vector(2 downto 0); 	-- Señal para almacenar el carry de salida
+	 signal f_carry : std_logic_vector(3 downto 0); 	-- Señal para almacenar el carry de salida
+	 signal result: std_logic_vector(3 downto 0);
 	 
 	 
 begin
@@ -40,12 +45,12 @@ begin
     FA2: sumador1Bit port map (A(2), B(2), f_carry(1), S(2), f_carry(2)); -- Suma el tercer bit con el acarreo anterior
     FA3: sumador1Bit port map (A(3), B(3), f_carry(2), S(3), Cout); -- Suma el bit más significativo y obtiene el acarreo de salida
 	 
-	 --f_carry <= "000" & C_int(3);	--Guarda el carry de salida como un número de 4 bits
+	 S <= result;
 	 
-	 --Conversor de las salidas a números de 7 bits correspondientes
-	 --a cada segmento de los display utilizados
-	 
-	 --hex1: disp7seg port map(result, sum4);  --Convierte el resultado de la suma en un número de 7 bits 
-	 --hex2: disp7seg port map(f_carry, Cout);	  --Convierte el carry final en un número de 7 bits 
+	 -- Instancia del decodificador de 7 segmentos para la suma
+    sum_disp: disp7seg port map (result, sum_seg);
+
+    -- Instancia del decodificador de 7 segmentos para el acarreo
+    carry_disp: disp7seg port map (f_carry, carry_seg);
 	 
 end sumadorCompleto4Bits;
