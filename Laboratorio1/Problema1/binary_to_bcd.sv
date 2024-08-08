2d3559be479d047bc2689823d3e0959deb794b3e
Giancarlo Vega Marin <jungianca6@estudiantec.cr>
2024-08-08 00:56:58 -0600

    Update binary_to_bcd.sv

--- Laboratorio1/Problema1/binary_to_bcd.sv
+++ Laboratorio1/Problema1/binary_to_bcd.sv
@@ -8,24 +8,24 @@ module binary_to_bcd(
 			
   always_comb 
    case(binary)     
-    4'b0000: bcd = 6'b000000; //0 en BCD
-    4'b0001: bcd = 6'b000001; //1 en BCD
-    4'b0010: bcd = 6'b000010; //2 en BCD
-    4'b0011: bcd = 6'b000011; //3 en BCD
-    4'b0100: bcd = 6'b000100; //4 en BCD  
-    4'b0101: bcd = 6'b000101; //5 en BCD
-    4'b0110: bcd = 6'b000110; //6 en BCD
-    4'b0111: bcd = 6'b000111; //7 en BCD
-    4'b1000: bcd = 6'b001000; //8 en BCD
-    4'b1001: bcd = 6'b001001; //9 en BCD
-    4'b1010: bcd = 6'b010000; //10 en BCD
-    4'b1011: bcd = 6'b010001; //11 en BCD
-    4'b1100: bcd = 6'b010010; //12 en BCD
-    4'b1101: bcd = 6'b010011; //13 en BCD
-    4'b1110: bcd = 6'b010100; //14 en BCD
-    4'b1111: bcd = 6'b010101; //15 en BCD
+    4'b0000: bcd = 8'b00000000; //0 en BCD
+    4'b0001: bcd = 8'b00000001; //1 en BCD
+    4'b0010: bcd = 8'b00000010; //2 en BCD
+    4'b0011: bcd = 8'b00000011; //3 en BCD
+    4'b0100: bcd = 8'b00000100; //4 en BCD  
+    4'b0101: bcd = 8'b00000101; //5 en BCD
+    4'b0110: bcd = 8'b00000110; //6 en BCD
+    4'b0111: bcd = 8'b00000111; //7 en BCD
+    4'b1000: bcd = 8'b00001000; //8 en BCD
+    4'b1001: bcd = 8'b00001001; //9 en BCD
+    4'b1010: bcd = 8'b0001_0000; //10 en BCD
+    4'b1011: bcd = 8'b0001_0001; //11 en BCD
+    4'b1100: bcd = 8'b0001_0010; //12 en BCD
+    4'b1101: bcd = 8'b0001_0011; //13 en BCD
+    4'b1110: bcd = 8'b0001_0100; //14 en BCD
+    4'b1111: bcd = 8'b0001_0101; //15 en BCD
 
-    default: bcd = 6'b110000;
+    default: bcd = 8'b11110000;
    endcase
 	
 endmodule
